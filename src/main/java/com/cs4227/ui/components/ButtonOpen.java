0563daf329292e5a06001af1213fc581eb1036d0
Shane Mckenna <ShaneMckenna23@hotmail.co.uk>
2017-11-04 15:43:06 +0000

    Added command to open button

--- src/main/java/com/cs4227/ui/components/ButtonOpen.java
+++ src/main/java/com/cs4227/ui/components/ButtonOpen.java
@@ -1,10 +1,18 @@
 package com.cs4227.ui.components;
 
+import com.cs4227.ui.commands.OpenCommand;
+
 import javax.swing.*;
 
 class ButtonOpen extends JButton {
 
+    OpenCommand openCommand = new OpenCommand();
+
     public ButtonOpen(String name) {
         super(name);
+    }
+
+    public void onClick(){
+        openCommand.execute();
     }
 }
