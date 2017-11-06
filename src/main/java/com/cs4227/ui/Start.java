c63696be4373212eb943b2c0d2fa0fe537ebe87c
Shane Mckenna <ShaneMckenna23@hotmail.co.uk>
2017-11-06 14:41:17 +0000

    Added toolbox view

--- src/main/java/com/cs4227/ui/Start.java
+++ src/main/java/com/cs4227/ui/Start.java
@@ -2,9 +2,9 @@ package com.cs4227.ui;
 
 import com.cs4227.ui.views.ImageView;
 import com.cs4227.ui.views.OptionsView;
+import com.cs4227.ui.views.ToolboxView;
 
 import javax.imageio.ImageIO;
-import javax.swing.*;
 import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
@@ -13,9 +13,10 @@ import java.io.File;
 
 public class Start {
 
-    public static boolean isImageOpen;
+    public static boolean isImageOpen,isToolsOpen;
     public static OptionsView optionsView;
     public static ImageView imageView;
+    public static ToolboxView toolboxView;
 
     public static void main(String[] args) throws Exception {
 
@@ -41,8 +42,19 @@ public class Start {
             }
         });
         optionsView.setSize(600, 80);
-        optionsView.setLocation(dim.width/2-imageView.getSize().width/2, dim.height/2-imageView.getSize().height/2);
+        optionsView.setLocation(dim.width/2-imageView.getSize().width/2, dim.height/2-imageView.getSize().height/2 - 100);
         optionsView.setVisible(true);
+
+        toolboxView = new ToolboxView();
+        isToolsOpen = true;
+
+        toolboxView.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                isToolsOpen = false;
+            }});
+        toolboxView.setSize(220, 500);
+        toolboxView.setLocation((1300), (250));
+        toolboxView.setVisible(true);
     }
 
     public static void refreshImage (BufferedImage image, int w, int h) throws Exception {
--- src/main/java/com/cs4227/ui/views/ToolboxView.java
+++ src/main/java/com/cs4227/ui/views/ToolboxView.java
@@ -1,4 +1,48 @@
 package com.cs4227.ui.views;
 
-public class ToolboxView {
-}
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.io.IOException;
+
+public class ToolboxView extends JFrame {
+
+    private static JComboBox dropdownFilterType;
+    private static JLabel labelFilterType;
+
+    public ToolboxView() throws IOException {
+        super("Adjustments");
+
+        JPanel buttonPanel = new JPanel();
+
+        GridBagLayout gridbag = new GridBagLayout();
+        buttonPanel.setLayout(gridbag);
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        gbc.insets.top = 5;
+        gbc.insets.bottom = 5;
+        gbc.insets.left = 5;
+        gbc.insets.right = 5;
+
+        labelFilterType = new JLabel("Filter Type:");
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gridbag.setConstraints(labelFilterType, gbc);
+
+        dropdownFilterType = new JComboBox();
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        buttonPanel.add(dropdownFilterType,gbc);
+
+
+
+
+
+
+        //Add the buttons and the log to the frame
+        Container contentPane = getContentPane();
+        contentPane.add(buttonPanel, BorderLayout.NORTH);
+    }
+}// end of class
