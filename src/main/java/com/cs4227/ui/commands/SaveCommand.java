6f741b82039ee27963defa2f73b4eab7588cbb9f
Shane Mckenna <ShaneMckenna23@hotmail.co.uk>
2017-11-06 15:47:05 +0000

    Added Save Command

--- src/main/java/com/cs4227/ui/commands/SaveCommand.java
+++ src/main/java/com/cs4227/ui/commands/SaveCommand.java
@@ -1,10 +1,25 @@
 package com.cs4227.ui.commands;
 
+import com.cs4227.ui.components.ImageSaver;
+
+import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 public class SaveCommand implements Command {
 
     @Override
     public void execute() {
-        System.out.print("Save File");
+        JFrame frame = new ImageSaver();
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+
+        frame.pack();
+        // frame.setSize(450, 300); // get image size and scale it
+        frame.setVisible(true);
     }
 
     @Override
